f60519467be3ac04c815e862dde9de5a93041760
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-02-29 20:41:51 +0100

    Unified artist and album

---
+++ album.js
@@ -0,0 +1,182 @@
+const albumContent = document.getElementById("album-content");
+
+const tableContent = document.getElementById("dynamicRows");
+
+let countTrack = 1;
+
+const endpoint = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+window.onload = getFromApi()
+
+async function getFromApi() {
+    try {
+        const results = await fetch(`${endpoint}${id}`);
+        const json = await results.json();
+        console.log(json)
+        createHtml(json)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function createHtml(album) {
+    // parte superiore artista album
+
+    let topAlbumCont = document.createElement("div");
+    topAlbumCont.classList.add("d-flex", "mt-5", "container-fluid", "align-items-center");
+
+    let imgCont = document.createElement("div");
+
+    let albumImg = document.createElement("img");
+    albumImg.src = album.cover_big;
+    albumImg.classList.add("img-fluid", "album-img");
+
+    let infoCont = document.createElement("div");
+    infoCont.classList.add("ms-3")
+
+    let albumSub = document.createElement("h6");
+    albumSub.innerText = "ALBUM";
+    let albumTitle = document.createElement("h2");
+    albumTitle.innerText = album.title;
+    albumTitle.style.fontSize = "50px";
+
+    let artistInfoCont = document.createElement("div");
+    artistInfoCont.style.marginTop = "70px";
+    artistInfoCont.style.position = "absolute";
+
+
+    let artistImg = document.createElement("img");
+    artistImg.src = album.artist.picture;
+    artistImg.classList.add("artist-img");
+
+    let artistTitle = document.createElement("span");
+    artistTitle.innerText = ` ${album.artist.name}·${album.release_date}·${album.nb_tracks} brani, ${album.duration} minuti`;
+
+    artistInfoCont.appendChild(artistImg);
+    artistInfoCont.appendChild(artistTitle);
+
+    imgCont.appendChild(albumImg);
+
+    infoCont.appendChild(albumSub)
+    infoCont.appendChild(albumTitle);
+    infoCont.appendChild(artistInfoCont);
+
+
+    topAlbumCont.appendChild(imgCont);
+    topAlbumCont.appendChild(infoCont);
+
+    albumContent.appendChild(topAlbumCont);
+
+
+    // barra comandi
+
+    let middleAlbumCont = document.createElement("div");
+    middleAlbumCont.classList.add("d-flex", "mt-3", "align-items-center");
+
+    let playBtnCont = document.createElement("div");
+
+    let playBtn = document.createElement("i");
+    playBtn.classList.add("fa-solid", "fa-circle-play", "p-3", "me-2");
+    playBtn.style.color = "#1ed75f";
+    playBtn.style.fontSize = "50px";
+    playBtn.style.cursor = "pointer";
+
+    let groupBtn = document.createElement("div");
+
+    let heartBtn = document.createElement("i");
+    heartBtn.classList.add("fa-regular", "fa-heart", "p-3");
+    heartBtn.style.fontSize = "35px";
+    heartBtn.style.cursor = "pointer";
+
+    let downloadBtn = document.createElement("i");
+    downloadBtn.classList.add("fa-solid", "fa-circle-arrow-down", "p-3");
+    downloadBtn.style.fontSize = "35px";
+    downloadBtn.style.cursor = "pointer"
+
+    let dotsBtn = document.createElement("i");
+    dotsBtn.classList.add("fa-solid", "fa-ellipsis", "p-3");
+    dotsBtn.style.fontSize = "35px";
+    downloadBtn.style.cursor = "pointer";
+
+    playBtnCont.appendChild(playBtn);
+
+    groupBtn.appendChild(heartBtn);
+    groupBtn.appendChild(downloadBtn);
+    groupBtn.appendChild(dotsBtn);
+
+    middleAlbumCont.appendChild(playBtnCont);
+    middleAlbumCont.appendChild(groupBtn);
+
+    albumContent.appendChild(middleAlbumCont);
+
+    // tabella con canzoni album
+
+    let allTracks = album.tracks.data;
+    allTracks.forEach((track) => {
+        createTable(track)
+    })
+
+
+}
+
+function createTable(track) {
+    let tRows = document.createElement("tr");
+
+    let trackNum = document.createElement("th");
+    trackNum.innerText = countTrack;
+    countTrack++;
+
+    let trackTitleCont = document.createElement("td");
+    trackTitleCont.style.cursor = "pointer";
+
+    let trackTitleBox = document.createElement("div");
+    trackTitleBox.innerHTML = `${track.title}<br>${track.artist.name}`;
+
+    trackTitleCont.addEventListener("click", () => {
+        bottomSong(track.title, track.artist.name, track.album.cover_small)
+    })
+
+    trackTitleCont.appendChild(trackTitleBox);
+
+    let trackRank = document.createElement("td");
+    trackRank.innerText = track.rank;
+
+    let trackDuration = document.createElement("td");
+    trackDuration.innerText = `${track.duration} secondi`;
+
+    tRows.appendChild(trackNum);
+    tRows.appendChild(trackTitleCont);
+    tRows.appendChild(trackRank);
+    tRows.appendChild(trackDuration);
+
+    tableContent.appendChild(tRows);
+}
+
+function bottomSong(title, artist, albumImg) {
+    const bottomBarSong = document.getElementById("song-content");
+
+    bottomBarSong.innerHTML = "";
+    let bottomCont = document.createElement("div");
+    bottomCont.classList.add("d-flex", "align-items-center", "p-3")
+
+    let img = document.createElement("img");
+    img.src = albumImg;
+    img.style.height = "50px";
+
+    let infoArtist = document.createElement("span");
+    infoArtist.innerHTML = `${title}<br>${artist}`;
+    infoArtist.classList.add("ms-3");
+
+    let icon = document.createElement("i");
+    icon.classList.add("fa-regular", "fa-heart", "ms-4");
+
+    bottomCont.appendChild(img);
+    bottomCont.appendChild(infoArtist);
+    bottomCont.appendChild(icon)
+
+    bottomBarSong.appendChild(bottomCont);
+
+
+}
---
+++ script_artist.js
@@ -0,0 +1,119 @@
+//tramite ID dell'album su cui l'utente clicca
+
+// id esempio (pink floyd)
+let artistID = 860;
+
+//URL 
+const endpoint = `https://striveschool-api.herokuapp.com/api/deezer/artist/`;
+const params = new URLSearchParams(window.location.search)
+const id = params.get("id")
+console.log(id)
+
+//NODI UTILI
+//sezione copertina:
+const jumbotrone = document.getElementById("jumboTitle"); 
+//sezione tabella risultati: 
+const artistTable = document.getElementById("artistTable");
+//sezione favourite songs:
+const favSongSection = document.getElementById("favSongSection");
+
+window.onload = getArtist ()
+
+//fetch GET artista 
+async function getArtist () {
+    try {
+        let response = await fetch(endpoint+id);
+        let object = await response.json();
+        console.log(object);
+        createTitlePage (object);
+        getTracks(object);
+        innerFavourite(object);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// funzione che mostra i dati nel jumbotron: 
+function createTitlePage ({name, picture_xl}) {
+    let pTitle = document.querySelector("#jumboTitle p");
+    pTitle.innerText = `${name}`;
+
+    let coverImg = document.querySelector("#jumboTitle");
+    coverImg.style.backgroundImage = `url(${picture_xl})`
+    coverImg.classList.add("jumbotron");
+}
+
+// funzione che richiama endpoint per tracklist:
+async function getTracks ({tracklist}) {
+    try {
+        let response = await fetch(tracklist);
+        let object = await response.json();
+
+        let [one, two, three, four, five] = object.data; // voglio solo le prime 5
+        let tracksToShow = [];
+        tracksToShow.push(one, two, three, four, five);
+
+        tracksToShow.forEach(track => {
+            showTracks(track);
+
+        })
+        
+    } catch (error) {
+        console.log("There's an error!")
+    }
+}
+
+// funzione che mostra tracklist in table:
+// li number: 
+let listNumber = 1;
+function showTracks ({album, title, duration, rank}) {
+
+    //tableTr
+    let tableTr = document.createElement("tr");
+    tableTr.classList.add("border-0");
+    artistTable.appendChild(tableTr);
+
+    //tableTh
+    let tableTh = document.createElement("th");
+    let tableThNumber = listNumber;
+    listNumber++;
+    tableTh.classList.add("text-white", "px-2")
+    tableTh.innerText = `${tableThNumber}`;
+
+    //tableTD
+    let firstTableTd = document.createElement("td");
+    let albumPrev = document.createElement("img");
+    albumPrev.classList.add("img-tracklist");
+    albumPrev.src = `${album.cover}`;
+    let titleTrack = document.createElement("span");
+    titleTrack.classList.add("text-white", "fw-bold");
+    titleTrack.innerText = `${title}`;
+
+    let secondTableTd = document.createElement("td");
+    secondTableTd.classList.add("text-white", "ps-2");
+    secondTableTd.innerText = `${rank}`;
+    
+
+    let thirdTableTd = document.createElement("td");
+    thirdTableTd.classList.add("text-white");
+    thirdTableTd.innerText = `${duration}`;
+
+    tableTr.appendChild(tableTh);
+    tableTr.appendChild(firstTableTd);
+    firstTableTd.append(albumPrev, titleTrack);
+    tableTr.appendChild(secondTableTd);
+    tableTr.appendChild(thirdTableTd);
+}
+
+
+//funzione che aggiorna la sezione "Brani che ti piacciono":
+function innerFavourite ({name, picture}) {
+    let favImg = document.querySelector("#favSongSection img");
+    favImg.classList.add("img-favourite", "mx-3");
+    favImg.src = `${picture}`;
+    favImg.alt = "artist_image";
+
+    let favSpan = document.querySelector("#favSongSection div span:last-of-type");
+    favSpan.innerText = `Di ${name}`;
+}
